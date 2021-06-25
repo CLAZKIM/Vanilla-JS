aa2550823542062334e003b44e43627c8c4da407
CLAZKIM <kwt9121@gmail.com>
2021-06-25 23:35:07 +0900

    Update.Array

---
+++ momentum/app.js
@@ -0,0 +1,14 @@
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek)
+
+const toBuy = ["poteato", "tomato", "pizza"];
+toBuy.push("kimbab");
+console.log(toBuy[6]);
