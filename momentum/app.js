ca8140a3f2c26f5eafb2225e1dfd417454e931f2
CLAZKIM <kwt9121@gmail.com>
2021-07-02 10:50:17 +0900

    Events

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,40 @@
-const player =  {
-    name : "kwt",
-    sayHello : function(otherPersonName) {
-        console.log("hello " + otherPersonName + " nice to meet you!")
-    }
-};
+const h1 = document.querySelector("div.hello:first-child h1");
 
+console.dir(h1);
 
-player.sayHello("fsk");
+function handleH1Click() {
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    h1.innerText = "Mouse is here!"
+}
+
+function handleMouseLeave() {
+    h1.innerText = "Mouse is gone!"
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOOD")
+}
+
+h1.addEventListener("click", handleH1Click);
+h1.addEventListener("mouseenter",handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
