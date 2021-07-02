2c0b80b2161c810840c1e5efe61057e02d195eb8
CLAZKIM <kwt9121@gmail.com>
2021-07-02 14:57:37 +0900

    Hidden&`${}`

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) { // 첫번째 인자는 항상 이벤트 객체 정보를 받아온다
     event.preventDefault(); // 브라우저의 기본 동작을 막아준다.
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
