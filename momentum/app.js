be3010e592c8f4435f338e179685a44725b45248
CLAZKIM <kwt9121@gmail.com>
2021-07-02 13:00:02 +0900

    Toggle

--- momentum/app.js
+++ momentum/app.js
@@ -3,38 +3,8 @@ const h1 = document.querySelector("div.hello:first-child h1");
 console.dir(h1);
 
 function handleH1Click() {
-    h1.style.color = "blue";
+    h1.classList.toggle("clicked");
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!"
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!"
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOOD")
-}
 
 h1.addEventListener("click", handleH1Click);
-h1.addEventListener("mouseenter",handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
