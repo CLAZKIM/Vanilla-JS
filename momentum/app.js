d3c8f550ab7fd15411b45f6c521e2a3a1a3f3729
CLAZKIM <kwt9121@gmail.com>
2021-07-02 15:53:09 +0900

    CreateLogin

--- momentum/app.js
+++ momentum/app.js
@@ -3,14 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) { // 첫번째 인자는 항상 이벤트 객체 정보를 받아온다
     event.preventDefault(); // 브라우저의 기본 동작을 막아준다.
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit);
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
